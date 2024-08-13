36685aa9be4eb11f233708132ad7d8b7d35172f0
Andrea Aguirre Ruiz <aguirreandrea457@gmail.com>
2024-08-13 17:25:46 -0500

    avance

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import Fondo from "../assets/fondo.jpg";
 import "../Style/Login.css";
+import { InputText } from 'primereact/inputtext';
 
 export default function Login() {
+    const [value, setValue] = useState('');
+
     return(
         <>
         <div className="container_login">
             <img src={Fondo} alt="Fondo agua" />
             <div className="container_square">
-                
+                <InputText value={value} onChange={(e) => setValue(e.target.value)} />
             </div>
         </div>
         
