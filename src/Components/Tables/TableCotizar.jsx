61799a8759036f44d98f48b7e6fe89989b92b8d7
Andrea Aguirre Ruiz <aguirreandrea457@gmail.com>
2024-09-11 16:57:55 -0500

    avance

--- src/Components/Tables/TableCotizar.jsx
+++ src/Components/Tables/TableCotizar.jsx
@@ -4,7 +4,7 @@ import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 
 export default function TableCotizar() {
-  const [products, setProducts] = useState([]);
+  const [products, ] = useState([]);
 
   return (
     <>
