60ca564060a7ac1013f9bc94bf19eef54f2b0e2f
AndreaC05 <aguirreandrea457@gmail.com>
2024-10-07 17:19:37 -0500

    avance

--- src/Pages/Productos.jsx
+++ src/Pages/Productos.jsx
@@ -1,5 +1,6 @@
 import SideBar from "../Components/SideBar";
 import ProductosCom from "../Components/ProductosCom";
+import { Button } from "primereact/button";
 import "../Style/Productos.css";
 
 export default function Productos() {
@@ -8,6 +9,14 @@ export default function Productos() {
       <div className="header flex align-items-center">
         <SideBar />
         <h2>Inventario</h2>
+      </div>
+      <div className="botones_scanner flex p-1">
+        {/* <div className="camara_cel mr-3">
+          <Button icon="pi pi-instagram" className="p-1"/>
+        </div> */}
+        <div className="maquina_scanner">
+          <Button icon="pi pi-barcode" className="p-1"/>
+        </div>
       </div>
       <div className="container_productos_principal">
         <ProductosCom />
